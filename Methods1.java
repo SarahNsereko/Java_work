0b7f3069029924ed29bc594eb14d483e2bbea8b8
Sarah Nsereko <107504333+SarahNsereko@users.noreply.github.com>
2022-10-06 21:06:07 +0300

    Add files via upload

---
+++ Methods1.java
@@ -0,0 +1,20 @@
+public class Methods1{
+    public static void main(String[] args){
+        multiply(5,10);
+        multiply(3,2);
+        welcome();
+        divide(4,2);
+
+    }
+    public static void multiply(int a, int b ){//methods
+        System.out.println(a*b);
+
+    }
+    public static void welcome(){
+        System.out.println("Hi,welcome,How are you");
+    }
+    public static void divide(int h,int k){
+        System.out.println(h/k);
+
+    }
+        }
