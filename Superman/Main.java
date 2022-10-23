31d13718aa90420b6ad0bea0a9985ea2a2fa316b
Sarah Nsereko <107504333+SarahNsereko@users.noreply.github.com>
2022-10-23 09:38:03 +0300

    Add files via upload

---
+++ Superman/Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args){
+        Car c = new Car();
+        System.out.println(c.speed);
+        c.drive();    }
+}
---
+++ Superman/Vehicle.java
@@ -0,0 +1,24 @@
+class Vehicle {
+    int speed=120;
+     // super Is a keyword in java that refers to the reference class in an inheritance relationship 
+     Vehicle(){
+        System.out.println("Lets move faster in the vehicle constrictor");
+     }
+     void drive(){
+        System.out.println("Drive");
+     }
+}
+
+//If you have multiple classes take out the public word 
+class Car extends Vehicle {
+    Car(){
+        super();//calls the constructor  in the parent class
+    }
+   
+    void drive(){
+        
+         //System.out.println("Driving fast");
+         super.drive();// super helps us get a method from the parent class
+    }
+//If the program has two methods that are the same we can use the super.drive to prioritise the main method
+}
